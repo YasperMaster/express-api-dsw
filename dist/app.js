d911c06dc20a94be7b68036d1a4e0b273bc21252
Tomas Yasparra <tomasyas@gmail.com>
2024-07-09 10:25:28 -0300

    create get user by id

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,13 @@ const users = [
 app.get("/api/users", (req, res) => {
     res.json(users);
 });
+app.get("/api/users/:id", (req, res) => {
+    const user = users.find((user) => user.id === req.params.id);
+    if (!user) {
+        res.status(404).send({ message: "User not found" });
+    }
+    res.json(user);
+});
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000/");
 });
