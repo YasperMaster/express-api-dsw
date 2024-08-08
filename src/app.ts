13b3af9644550513bf165b8e0006669984e322fc
Tomas Yasparra <tomasyas@gmail.com>
2024-08-08 08:03:57 -0300

    add findAll, findOne, add, update & remove to user.controller.ts

--- src/app.ts
+++ src/app.ts
@@ -1,98 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import { User } from "./user/user.entity.js";
 import { UserRepository } from "./user/user.repository.js";
+import { userRouter } from "./user/user.routes.js";
 
 const app = express()
 app.use(express.json())
 
-const repository = new UserRepository()
-
-const users = [
-    new User(
-        'Tomas Yasparra',
-        "tomas@gmail.com",
-        "01/05/2001",
-        "contrasenia123",
-        'a02b91bc-3769-4221-beb1-d7a3aeba7dad'
-    ),
-]
-
-function sanitizeUserInput(req: Request, res: Response, next: NextFunction) {
-
-    req.body.sanitizeUserInput = {
-        name: req.body.name,
-        email: req.body.email,
-        pass: req.body.pass,
-        birthdate: req.body.birthdate,
-    }
-    //faltan validaciones
-
-    Object.keys(req.body.sanitizeUserInput).forEach(key => {
-        if(req.body.sanitizeUserInput[key] === undefined){
-        delete req.body.sanitizeUserInput[key]
-        }
-    })
-    next()
-}
-
-app.get("/api/users", (req, res) => {
-    res.json({data: repository.findAll() })
-})
-
-app.get("/api/users/:id", (req, res) => {
-    const id = req.params.id
-    const user = repository.findOne({id})
-    if(!user){
-       return res.status(404).send({ message:"User not found" })
-    }
-    res.json({data: user})
-})
-
-app.post("/api/users", sanitizeUserInput, (req, res) => {
-    const input = req.body.sanitizeUserInput
-
-    const userInput = new User (
-        input.name, 
-        input.email, 
-        input.pass, 
-        input.birthdate)
-
-    const user = repository.add(userInput)
-    return res.status(201).send({ message: "User created", data: user })
-})
-
-app.put("/api/users/:id", sanitizeUserInput, (req, res) => {
-    req.body.sanitizeUserInput.id = req.params.id
-    const user = repository.update(req.body.sanitizeUserInput)
-
-    if(!user){
-        return res.status(404).send({ message: "User not found" })
-    }
-
-    return res.status(200).send({ message: "User updated succesfully" ,data: users })
-})
-
-app.patch("/api/users/:id", sanitizeUserInput, (req, res) => {
-    req.body.sanitizeUserInput.id = req.params.id
-    const user = repository.update(req.body.sanitizeUserInput)
-
-    if(!user){
-        return res.status(404).send({ message: "User not found" })
-    }
-
-    return res.status(200).send({ message: "User updated succesfully" ,data: users })
-})
-
-app.delete("/api/users/:id", (req, res) => {
-    const id = req.params.id
-    const user = repository.delete({id})
-
-    if(!user){
-        res.status(404).send({ message: "User not found" })
-    }else {
-    res.status(200).send({ message: "User deleted succesfully" })
-    }
-})
+app.use("/api/users", userRouter)
 
 app.use((req, res) => {
     res.status(404).send({ message: "Resource not found" })
